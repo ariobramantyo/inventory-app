7e62f37facff4e46097f27942f8c2f92d3ea6c9e
Ario Bagus Bramantyo <i.ario.bramantyo@gmail.com>
2022-10-06 20:54:19 +0700

    first commit

---
+++ lib/views/home_page.dart
@@ -0,0 +1,78 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:inventory_app/models/inventory.dart';
+import 'package:inventory_app/providers/firestore_service.dart';
+import 'package:inventory_app/views/inventory_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Inventory App'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(
+              builder: (context) {
+                return const InventoryPage();
+              },
+            ));
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+          stream:
+              FirebaseFirestore.instance.collection('inventories').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              var inventories = snapshot.data!.docs
+                  .map((inventory) => Inventory.fromSnapshot(inventory))
+                  .toList();
+
+              return ListView.builder(
+                itemCount: inventories.length,
+                itemBuilder: (context, index) {
+                  var id = snapshot.data!.docs[index].id;
+
+                  return ListTile(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) {
+                          return InventoryPage(
+                            inventory: inventories[index],
+                            id: id,
+                          );
+                        },
+                      ));
+                    },
+                    title: Text(inventories[index].name),
+                    subtitle: Text(inventories[index].description),
+                    trailing: IconButton(
+                        onPressed: () {
+                          FirestoreService.deleteInvetory(id);
+                        },
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        )),
+                  );
+                },
+              );
+            }
+
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ));
+  }
+}
+
+// Column(
+//           children: [
+//             
+//           ],
+//         )
